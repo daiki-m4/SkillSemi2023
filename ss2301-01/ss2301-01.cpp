ddd7734437bfd6cdc2561041c05a0a81e62b6f99
daiki <148743937+daiki-m4@users.noreply.github.com>
2023-11-15 15:51:00 +0900

    例1

--- ss2301-01/ss2301-01.cpp
+++ ss2301-01/ss2301-01.cpp
@@ -1,12 +1,19 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
-using namespace std;
 
+template<typename T>
+T minimum(T value) {
+    return value;
+}
+
+template<typename T, typename... Args>
+T minimum(T first, Args... args) {
+    T rest = minimum(args...);
+    return (first < rest) ? first : rest;
+}
 
 int main() {
-      
+    double result = minimum(3.2, 4.7, 2.1, 5.6);
+    std::cout << "最小値: " << result << std::endl;
+
     return 0;
 }
